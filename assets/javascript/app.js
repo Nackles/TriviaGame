e03ab0f928bf2f03f5ec83a697a2dffa3de017e4
Nackles <45973617+Nackles@users.noreply.github.com>
2019-03-07 21:08:59 -0800

    safety push - bootstrap html online

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+console.log("don't forget the timer my friend");
+
+//on document.ready:
+
+//variable timer = setTimer()*60000;
+//when time runs out {
+//function for checkAnswers fires, html changes to stats page with a reset button
+//}
+
+//function checkAnswers(){}
+
+//function reset(){}
+
+
+// Unfairly Random Facts
+
+// What is the third-highest grossing movie of all time?
+// *Star Wars: The Force Awakens
+// Black Panther
+// Frozen
+// Zootopia
+
+// Which video game is best?
+// *Dead Rising
+// *Apex Legends
+// *Any Zelda game
+// *Some other game
+
+// Where did Toby Fox create Undertale?
+// *Someone else's basement
+// Their house
+// An office
+// A studio apartment
+
+// How many times does the Earth get hit by lightning in one year?
+// 136,000
+// 3,000
+// *Like 3 billion
+// 50,789,144
+
+// In Finding Nemo, what kind of fish is Dory?
+// *Regal Blue Tang
+// Tiger Shark
+// Short-term Bluegill
+// Elephant Plankton
